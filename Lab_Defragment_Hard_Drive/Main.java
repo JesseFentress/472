eae6ae24b0e64a63f104efdfa8ab6d24c0e47c42
Jesse Fentress <jessefentress246@gmail.com>
2023-03-10 11:57:52 -0500

    Midterm finished.

--- Lab_Defragment_Hard_Drive/Main.java
+++ Lab_Defragment_Hard_Drive/Main.java
@@ -1,100 +1,119 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
 
-    static char harddrive[] = { 'A', 'B', 'B', ' ', 'D', ' ', 'X', 'X', ' ', 'E', 'F', 'G', ' ', 'H', 'H', 'J', 'J',
-            ' ',
-            ' ', ' ', 'J', 'K', 'K', 'J', 'L', 'L', 'L', ' ', 'M', 'M', 'P', 'P', 'M', 'P', 'P', ' ', ' ', 'R', ' ',
-            ' ' };
-
-    static char CLEAR_SECTOR = ' ';
-
-    static Map<Character, ArrayList<Integer>> programs = new HashMap<Character, ArrayList<Integer>>();
+    static Map<Character, Program> programs = new HashMap<Character, Program>();
     static ArrayList<Integer> emptyMem = new ArrayList<>();
-    static Map<Integer, Character> locations = new HashMap<Integer, Character>();
+    static char EMPTY_SECTOR = ' ';
 
-    public static void checkContinuos() {
+    public static void findAllPrograms(Harddrive hdd) {
 
-    }
+        for (int i = 0; i < hdd.getHarddriveSize(); i++) {
 
-    public static void swap(Integer usedMemLocation, Integer emptyMemLocation) {
-
-        char currentChar = harddrive[usedMemLocation];
-
-        harddrive[emptyMemLocation] = harddrive[usedMemLocation];
-        harddrive[usedMemLocation] = CLEAR_SECTOR;
-
-        programs.get(currentChar).remove(usedMemLocation);
-        programs.get(currentChar).add(emptyMemLocation);
-
-        emptyMem.remove(emptyMemLocation);
-        emptyMem.add(usedMemLocation);
-
-    }
-
-    public static int getFirstAvailableSector() {
-        return emptyMem.get(0);
-    }
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-
-        for (int i = 0; i < harddrive.length; i++) {
-
-            char currentChar = harddrive[i];
-            locations.put(i, currentChar);
-            if (currentChar == CLEAR_SECTOR) {
+            if (hdd.isEmptySector(i)) {
                 emptyMem.add(i);
             } else {
+                char currentChar = hdd.getProgramAtSector(i);
                 if (programs.containsKey(currentChar)) {
-                    programs.get(currentChar).add(i);
+                    programs.get(currentChar).addSector(i);
+                    ;
                 } else {
-                    ArrayList<Integer> tempList = new ArrayList<Integer>();
-                    tempList.add(i);
-                    programs.put(currentChar, tempList);
+                    Program newProgram = new Program(currentChar);
+                    newProgram.addSector(i);
+                    programs.put(currentChar, newProgram);
                 }
             }
         }
+    }
 
-        System.out.println(emptyMem);
-        int index = 0;
-        int endPoint = 0;
+    public static void alterEmptyTable(int index1, int index2) {
+        emptyMem.remove(Integer.valueOf(index1));
+        emptyMem.add(Integer.valueOf(index2));
+    }
 
-        for (Map.Entry<Character, ArrayList<Integer>> program : programs.entrySet()) {
+    public static void alterProgramsTable(char programCharID, int index1, int index2) {
+        programs.get(programCharID).removeSector(Integer.valueOf(index1));
+        programs.get(programCharID).addSector(Integer.valueOf(index2));
+    }
 
-            index = program.getValue().get(0);
+    public static void printGreetingMessage() {
+        System.out.println("*********************************************************");
+        System.out.println("*** WELCOME TO THE HARDDRIVE DEFRAGMENTATION PROGRAM. ***");
+        System.out.println("*********************************************************");
+        System.out.println("*** This program defragments a virtual harddrive that ***");
+        System.out.println("*** holds of 13 fragmented programs. Upper-case ASCII ***");
+        System.out.println("*** characters are used to represent the programs.    ***");
+        System.out.println("*********************************************************");
+    }
 
-            if (index != 0 && index > endPoint + 1) {
-                System.out.println(programs);
-                swap(endPoint, getFirstAvailableSector());
-                System.out.println(programs);
-            }
+    public static void main(String[] args) {
 
-            int i = index;
+        printGreetingMessage(); // Greeting message about the program
 
-            for (int j = 0; j < program.getValue().size(); j++) {
+        Harddrive hdd = new Harddrive(); // Initialize the fragmented harddrive
 
-                int nextSectorOccupied = program.getValue().get(j);
-                System.out.println(nextSectorOccupied);
-                System.out.println(index + " " + i);
-                if (nextSectorOccupied > i) {
-                    System.out.println(program.getKey());
-                    int emptySector = getFirstAvailableSector();
-                    swap(i + 1, emptySector);
-                    swap(nextSectorOccupied, i + 1);
+        System.out.println("\n****************** Fragmented Harddrive *****************");
+
+        hdd.printHarddrive(); // Display the fragmented harddrive
+
+        findAllPrograms(hdd); // Identify where each program is stored
+
+        int sectorIndex = 0;
+
+        // Iterate over the programs found in the harddrive
+        for (Map.Entry<Character, Program> programEntry : programs.entrySet()) {
+
+            // Iterate for every sector the program is found
+            for (Integer sector : programEntry.getValue().getSectors()) {
+
+                // Character id of the program that is at our current sector
+                char programCharID = hdd.getProgramAtSector(sectorIndex);
+
+                // If the program entry we are at is not the same as the program we found
+                if (programCharID != programEntry.getValue().getProgramCharID()) {
+
+                    if (programCharID == EMPTY_SECTOR) { // Sector is empty
+
+                        // Swap the empty sector with the sector of the program
+                        hdd.swap(sector, sectorIndex);
+
+                        // Alter the empty table to represent the swap
+                        alterEmptyTable(sectorIndex, sector);
+
+                    } else { // The sector belongs to another program
+
+                        // Sort the empty memory so we have the first empty at the front
+                        Collections.sort(emptyMem);
+
+                        // Get the first available empty sector
+                        int firstAvailableSector = emptyMem.get(0);
+
+                        // Swap the program in the current sector with first empty sector
+                        hdd.swap(sectorIndex, firstAvailableSector);
+
+                        // Alter the tables to represent the swap
+                        alterProgramsTable(programCharID, sectorIndex, firstAvailableSector);
+                        alterEmptyTable(firstAvailableSector, sectorIndex);
+
+                        // Swap the program that belongs in the current sector with the newly empty
+                        // sector
+                        hdd.swap(sector, sectorIndex);
+
+                        // Alter the empty table to represent the swap
+                        alterEmptyTable(sectorIndex, sector);
+
+                    }
                 }
-                i++;
+                sectorIndex++; // Increment the sector index
             }
-            endPoint = i;
+
         }
 
-        System.out.println(harddrive);
-        // swap(4, 3);
-        System.out.println(harddrive);
-        // System.out.println(programs);
+        System.out.println("\n***************** Defragmented Harddrive ****************");
+
+        hdd.printHarddrive(); // Display defragmented harddrive
     }
 }
